28e5221c5f3bddd9f669d69c8aaa83d330e90c30
Neil van Vuuren <kalinith@gmail.com>
2026-07-05 11:16:02 +0200

    Ch4 L4 Profane added function to replace restricted words with ****

--- dist/handlers/validateChirp.js
+++ dist/handlers/validateChirp.js
@@ -1,13 +1,28 @@
+function wordCheck(chirp) {
+    const profaneWords = ["kerfuffle", "sharbert", "fornax"];
+    const words = chirp.split(" ");
+    const cleanWords = [];
+    for (const word of words) {
+        if (profaneWords.includes(word.toLowerCase())) {
+            cleanWords.push("****");
+        }
+        else {
+            cleanWords.push(word);
+        }
+    }
+    return cleanWords.join(" ");
+}
 export async function handlerValidateChirp(req, res) {
     const chirp = req.body;
     res.header("Content-Type", "application/json");
     if (chirp.body === null) {
         res.status(400);
-        res.send(JSON.stringify({ "error": "omething went wrong" }));
+        res.send(JSON.stringify({ "error": "Something went wrong" }));
     }
     else if (chirp.body.length < 140) {
+        const cleanChirp = wordCheck(chirp.body);
         res.status(200);
-        res.send(JSON.stringify({ "valid": true }));
+        res.send(JSON.stringify({ "cleanedBody": cleanChirp }));
     }
     else {
         res.status(400);
--- src/handlers/validateChirp.ts
+++ src/handlers/validateChirp.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
 
+type Profane = "kerfuffle" | "sharbert" | "fornax";
+
+function wordCheck(chirp: string): string {
+    const profaneWords: Profane[] = ["kerfuffle", "sharbert", "fornax"];
+    const words = chirp.split(" ");
+    const cleanWords: string[] = [];
+    for (const word of words) {
+        if (profaneWords.includes(word.toLowerCase() as Profane)) {
+            cleanWords.push("****");
+        } else {
+            cleanWords.push(word);
+        }
+    }
+    return cleanWords.join(" ");
+}
+
 export async function handlerValidateChirp(req: Request, res: Response): Promise<void> {
     type Chirp = {
         body: string;
@@ -9,10 +25,12 @@ export async function handlerValidateChirp(req: Request, res: Response): Promise
     res.header("Content-Type", "application/json");
     if (chirp.body === null) {
         res.status(400);
-        res.send(JSON.stringify({ "error": "omething went wrong" }));
+        res.send(JSON.stringify({ "error": "Something went wrong" }));
     } else if (chirp.body.length < 140) {
+        const cleanChirp = wordCheck(chirp.body)
+
         res.status(200);
-        res.send(JSON.stringify({ "valid": true }));
+        res.send(JSON.stringify({ "cleanedBody": cleanChirp }));
     } else {
         res.status(400);
         res.send(JSON.stringify({ "error": "Chirp is too long" }));
