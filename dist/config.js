5a9cad4f1a8035e0c561c431317102e55f82a913
Neil van Vuuren <kalinith@gmail.com>
2026-07-07 08:46:49 +0200

    Ch6 L3 Drizzle queries. added a users table and queries to select and insert.

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,12 @@
+process.loadEnvFile();
 export const config = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
+function envOrThrow(key) {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Missing environment variable: ${key}`);
+    }
+    return value;
+}
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
+process.loadEnvFile()
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+}
